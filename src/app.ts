547df545e24b130bee96eea7817f58cc30e49b8f
Ankit Chaurasia <Ankit.Chaurasia@Honeywell.com>
2020-02-09 18:48:16 +0530

    The TypeScript Compiler (and its Configuration)

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// let userInput: unknown;
+// let userName: string;
+
+// userInput = 5;
+// userInput = 'user';
+
+// // userName = userInput; // Error: Type 'unknown' is not assignable to type 'string'
+// //  To fix this
+// if (typeof userInput === 'string') {
+//   userName = userInput;
+// }
+
+// function generateError(message: string, code: number): never {
+//   throw { message, errorCode: code };
+// }
+
+// generateError('an error occurred', 500);
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', (event) => {
+  console.log('clicked');
+});
